2d80bf3c5b981e30cc7558e3c23db849bd63d9d9
Amir Ghorbani <arg7971@gmail.com>
2023-03-25 00:44:35 -0700

    added the Sankey chart

--- sankey_chart.js
+++ sankey_chart.js
@@ -1,10 +1,27 @@
+google.charts.load("current", { packages: ["sankey"] });
+var sankey_loaded = false;
+var sankey_chart = null;
+google.charts.setOnLoadCallback(function () {
+  sankey_loaded = true;
+  sankey_chart = new google.visualization.Sankey(
+    document.getElementById("sankey")
+  );
+  console.log("Sankey chart loaded");
+});
+
 const MAX_SHAPE_SELECTION = 5;
 //selected shapes on the map
 var shapes_selected = new Set();
 // we send these tokenized municipality names to the server
-var municipalities_selected = new Set();
+var selected_municipalities = new Set();
 var num_shape_selected = 0;
 var selected_year = "2017";
+var selected_factors = [
+  "alcohol_involved",
+  "driver_inattentive",
+  "drug_involved",
+  "impaired_involved",
+];
 
 const default_feature_style = {
   fillColor: "#ff7800",
@@ -13,20 +30,26 @@ const default_feature_style = {
   fillOpacity: 0.3,
 };
 
-var colors = ["#D81B1B", "#1ECEE5", "#FFC107", "#00334D", "#EC00FF"];
+[]
+var mun_colors = ["#D81B1B", "#1ECEE5", "#FFC107", "#00334D", "#EC00FF"];
+var factor_colors = ['#a6cee3', '#b2df8a', '#fb9a99', '#fdbf6f', '#cab2d6', '#ffff99', '#1f78b4', '#33a02c'];
 
 function shapeClicked(shape_prop, shape_color) {
   if (shapes_selected.has(shape_prop)) {
     // remove shape from selected shapes
+    shape_prop["mun_color"] = undefined;
     shapes_selected.delete(shape_prop);
-    colors.push(shape_color);
+    updatePlot();
+    mun_colors.push(shape_color);
     num_shape_selected--;
     return default_feature_style;
   } else if (num_shape_selected < MAX_SHAPE_SELECTION) {
     // add shape to selected shapes
-    shapes_selected.add(shape_prop);
     num_shape_selected++;
-    var _color = colors.pop();
+    var _color = mun_colors.pop();
+    shape_prop["mun_color"] = _color;
+    shapes_selected.add(shape_prop);
+    updatePlot();
     return {
       fillColor: _color,
       color: _color,
@@ -41,8 +64,130 @@ function shapeClicked(shape_prop, shape_color) {
 
 function handleYearChange(year) {
   selected_year = year.value;
+  updatePlot();
 }
 
 function alertMaximumShapeSelected() {
   alert("You can only select up to " + MAX_SHAPE_SELECTION + " municipalities");
+}
+
+function updatePlot() {
+  // convert selected shape properties to mun_tokens
+  selected_municipalities.clear();
+  shapes_selected.forEach((shape) => {
+    selected_municipalities.add(shape.mun_token);
+  });
+
+  console.log(
+    "Year: " +
+      selected_year +
+      ", mun_tokens: " +
+      Array.from(selected_municipalities) +
+      ", factors: " +
+      Array.from(selected_factors)
+  );
+
+  if(selected_municipalities.size == 0 || selected_factors.length == 0){
+    clearPlot();
+    console.log("clearing plot");
+    return;
+  }
+
+  var settings = {
+    url: "http://127.0.0.1:8000/getAccidents",
+    method: "POST",
+    timeout: 0,
+    xhrFields: {
+      withCredentials: true,
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: JSON.stringify({
+      year: selected_year,
+      mun_tokens: Array.from(selected_municipalities),
+      factors: selected_factors,
+    }),
+  };
+
+  console.log("POST >>> " + settings);
+  $.ajax(settings).done(function (response) {
+    if (response["status"] == 200) {
+      updateSankeyChart(response["res"]["rows"]);
+    }
+  });
+}
+
+function calculateNodeColors(){
+ var _node_colors = [];
+  var num_factors = selected_factors.length;
+  var _shapes_selected = Array.from(shapes_selected);
+  var num_municipalities = _shapes_selected.length;
+
+  //adding the first municipality node
+  var _mun_color = _shapes_selected[0].mun_color;
+  _node_colors.push(_mun_color);
+
+  //adding all the factor nodes 
+  for(var j = 0; j < num_factors; j++){
+    var _factor_color = factor_colors[j];
+    _node_colors.push(_factor_color);
+  }
+
+  //adding the rest of the municipality nodes
+  for(var i = 1; i < num_municipalities; i++){
+    _mun_color = _shapes_selected[i].mun_color;
+    _node_colors.push(_mun_color);
+  }
+
+
+
+  // get fill colors of selected shapes
+  // for(var i = 0; i < num_municipalities; i++){
+  //   var _mun_color = _shapes_selected[i].mun_color;
+  //   _node_colors.push(_mun_color);
+  //   for(var j = 0; j < num_factors; j++){
+  //     var _factor_color = factor_colors[j];
+  //     _node_colors.push(_factor_color);
+  //   }
+  // }
+  return _node_colors;
+}
+
+function calculateLinkColors(){
+
+}
+
+function updateSankeyChart(rows) {
+  if (!sankey_loaded) {
+    return;
+  }
+
+  clearPlot();
+  var data = new google.visualization.DataTable();
+  data.addColumn("string", "Municipality");
+  data.addColumn("string", "Contributing Factor");
+  data.addColumn("number", "Number of Accidents");
+  data.addRows(rows);
+
+    var options = {
+      height: 400,
+      sankey: {
+        node: {
+          colors: calculateNodeColors() 
+        },
+        link: {
+          colorMode: 'gradient'
+        }
+      }
+    };
+
+  sankey_chart.draw(data, options);
+}
+
+function clearPlot() {
+  if (!sankey_loaded) {
+    return;
+  }
+  sankey_chart.clearChart();
 }
--- server/server.py
+++ server/server.py
@@ -1,28 +1,86 @@
 from typing import Union
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from pathlib import Path
 
-dataset_path = Path(__file__).parent.parent / "Data/xlsx/TAS Accident (No Month Attribute) - Filtered.xlsx"
 
-# dataset = pd.read_excel(dataset_path)
+
+dataset_path = Path(__file__).parent.parent / "Data/xlsx/TAS Accident (No Month Attribute) - Filtered.xlsx"
+dataset = pd.read_excel(dataset_path)
 print(">>> loaded dataset")
 
+CODES = {
+    "SUCCESS": 200,
+    "INVALID": 400,
+}
+def convert_string(input_string):
+    words = input_string.split("_")
+    capitalized_words = [word.capitalize() for word in words]
+    output_string = " ".join(capitalized_words)
+    return output_string
+
+def get_sankey_data(year, mun_tokens, factors):
+    global dataset
+    
+    # Filter by year and mun_tokens
+    df = dataset[dataset['year'] == year]
+    df = df[df['mun_token'].isin(mun_tokens)]
+    
+    # Get frequency of datapoints for each mun_token and factor combination
+    rows = []
+    for mun_token in mun_tokens:
+        for factor in factors:
+            freq = len(df[(df['mun_token'] == mun_token) & (df[factor] == 'Yes')])
+            rows.append([mun_token, factor, freq])
+    
+    for row in rows:
+        row[0] = convert_string(row[0])
+        row[1] = convert_string(row[1])
+    
+    # Transform into JSON object
+    data = {
+        "year": year,
+        "rows": rows
+    }
+    return data
+
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
 
-@app.post("/getInformation")
-async def getInformation(info : Request):
-    req_info = await info.json()
-    return {
-        "status" : "SUCCESS",
-        "data" : req_info
-    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/getAccidents")
+async def getAccidents(info : Request):
+    req_info = await info.json()
+    print(req_info)
+
+    year = int(req_info['year'])
+    mun_tokens = req_info['mun_tokens']
+    factors = req_info['factors']
+
+   
+    if (year in [2017, 2018, 2019, 2020, 2021]) and  (len(mun_tokens) <= 5 and len(mun_tokens) > 0) and (len(mun_tokens) > 0):
+        res = get_sankey_data(year, mun_tokens, factors)
+        return {
+            "status" : CODES["SUCCESS"],
+            "res" : res
+        }
+    else: 
+        return {
+            "status" : CODES["INVALID"],
+            "res" : "Invalid request"
+        }
+
+   
